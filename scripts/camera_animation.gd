2601f8ab059538b73b69c4282a6ce09a3ff3c7be
GreenBlackSky <greenblacksky@gmail.com>
2022-06-06 13:07:47 +0300

    camera zoom

--- scripts/camera_animation.gd
+++ scripts/camera_animation.gd
@@ -1,9 +1,16 @@
 extends AnimationPlayer
 
 
-var current_level = 0
 const total_levels = 4
-var camera_speed = 1
+export(float) var camera_rotation_speed = 1
+export(float) var camera_zoom_speed = 25
+
+export(float) var camera_rotation_max = PI/4
+export(float) var camera_rotation_min = 0
+export(float) var camera_zoom_max = 13
+export(float) var camera_zoom_min = 5
+
+var current_level = 0
 var previous_mouse_position = Vector2()
 
 
@@ -15,25 +22,33 @@ func _process(delta):
 	if Input.is_action_pressed("camera_control"):
 		var mouse_position = get_viewport().get_mouse_position()
 		if previous_mouse_position and mouse_position != previous_mouse_position:
-			var diff = (previous_mouse_position - mouse_position)*camera_speed*delta
-			get_node("camera_pivot").rotate(Vector3(0, 1, 0), diff.x)
-			var camera_base = get_node("camera_pivot/camera_base")
+			var diff = (previous_mouse_position - mouse_position)*camera_rotation_speed*delta
+			$"camera_pivot".rotate(Vector3(0, 1, 0), diff.x)
+			var camera_base = $"camera_pivot/camera_base"
 			if (
-				(diff.y > 0 and camera_base.rotation.x < 0) or
-				(diff.y < 0 and camera_base.rotation.x > -PI/4)
+				(diff.y > 0 and camera_base.rotation.x < -camera_rotation_min) or
+				(diff.y < 0 and camera_base.rotation.x > -camera_rotation_max)
 			):
 				camera_base.rotate(Vector3(1, 0, 0), diff.y)
 		previous_mouse_position = mouse_position
 	else:
 		previous_mouse_position = Vector2()
+	if Input.is_action_just_released("camera_zoom_in"):
+		var camera = $"camera_pivot/camera_base/camera"
+		if camera.transform.origin.z > camera_zoom_min:
+			camera.translate(Vector3(0, 0, -camera_zoom_speed*delta))
+	if Input.is_action_just_released("camera_zoom_out"):
+		var camera = $"camera_pivot/camera_base/camera"
+		if camera.transform.origin.z < camera_zoom_max:
+			camera.translate(Vector3(0, 0, camera_zoom_speed*delta))
 
 
 func _animation_finished(anim_name):
 	if anim_name == "move_camera_up":
-		get_node("camera_pivot").translate(Vector3(0, 2, 0))
-		get_node("camera_pivot/camera_base").translate(Vector3(0, -2, 0))
+		$"camera_pivot".translate(Vector3(0, 2, 0))
+		$"camera_pivot/camera_base".translate(Vector3(0, -2, 0))
 		current_level -= 1
 	if anim_name == "move_camera_down":
-		get_node("camera_pivot/camera_base").translate(Vector3(0, 2, 0))
-		get_node("camera_pivot").translate(Vector3(0, -2, 0))
+		$"camera_pivot/camera_base".translate(Vector3(0, 2, 0))
+		$"camera_pivot".translate(Vector3(0, -2, 0))
 		current_level += 1
